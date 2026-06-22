123b1f4f8be4d15094fea17472582f9b6bc8cb04
Taala2 <bayirbekutaalay@icloud.com>
2026-06-22 15:56:39 +0600

    style(services): change style

--- users/services.py
+++ users/services.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 
 from core.security import hash_password, verify_password
 from users.models import User
-from users.repository import create, get_by_id, get_user_by_login_or_email, update
+from users.repository import create_user, get_user_by_id, get_user_by_login_or_email, update_user
 from users.schemas import UserRole
 
 
@@ -15,7 +15,7 @@ def create_user_service(db: Session, login: str, email: str, password: str):
                    email = email,
                    hashed_password = hash_password(password))
 
-    return create(db, new_user)
+    return create_user(db, new_user)
 
 def authenticate_user_service(db: Session, login_or_email: str, password: str):
     user = get_user_by_login_or_email(db, login_or_email, login_or_email)
@@ -29,14 +29,14 @@ def authenticate_user_service(db: Session, login_or_email: str, password: str):
     return user
 
 def change_user_role_service(db: Session, current_user: User, user_id: int, role: UserRole):
-    user = get_by_id(db, user_id)
+    user = get_user_by_id(db, user_id)
 
     if not user:
         return None
 
     if current_user.role == user.role:
         return None
-    
+
     user.role = role
 
-    return update(db, user)
+    return update_user(db, user)
